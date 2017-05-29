06da96c63a58872d57f5b15d3ad44fc1d5d9e3c1
joriwind <j.winderickx@gmail.com>
2017-05-29 16:02:57 +0200

    address conversion

--- examples/hecomm/otp-node/resources/res-compartner.c
+++ examples/hecomm/otp-node/resources/res-compartner.c
@@ -1,6 +1,7 @@
 #include "contiki.h"
 
 #include <string.h>
+#include <stdio.h>
 #include "contiki.h"
 #include "contiki-net.h"
 #include "ospad.h"
@@ -32,8 +33,8 @@ static void
 res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
     size_t len = 0;
-    uip_ipaddr_t *addr;
-    const uint8_t *bytes = NULL;
+    uip_ipaddr_t addr;
+    const char *bytes = NULL;
     uint16_t integers[8];
     int success = 0;
     int i;
@@ -42,18 +43,29 @@ res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t prefer
         if(len != 16){
             PRINTF("Wrong lenght of address: %i\n", len);
         }
-        PRINTF("Communication partner: %.*s\n", len, bytes);
+        //PRINTF("Communication partner: %.*s\n", len, bytes);
         //Conversion to u16
+        PRINTF("Remap \n");
+        /*uint8_t buf1;
+        uint8_t buf2;
         for (i = 0; i<8; i++){
-            integers[i] = bytes[2*i] << 8;
-            integers[i] |= bytes[2*i+1];
-        }
+            sscanf(&bytes[2*i], "%02x", &buf1);
+            sscanf(&bytes[2*i + 1], "%02x", &buf2);
+            integers[i] = buf1 << 8;
+            integers[i] |= buf2;
+            PRINTF("Integer %i: %i ", i, integers[i]);
+        }*/
+        uiplib_ip6addrconv(bytes, &addr);
+        PRINTF("\n");
+        PRINTF("Creating address \n");
         //Create address
-        uip_ip6addr(addr, integers[0], integers[1], integers[2], integers[3], integers[4], integers[5], integers[6], integers[7]);
+        //uip_ip6addr(&addr, integers[0], integers[1], integers[2], integers[3], integers[4], integers[5], integers[6], integers[7]);
         //Set communication partner in ospad
-        setCommunicationPartner(addr);
+        PRINTF("Setting address \n");
+        setCommunicationPartner(&addr);
         
-        PRINT6ADDR(addr);
+        PRINTF("Printing the address: \n");
+        PRINT6ADDR(&addr);
         success = 1;
     }
     if(!success) {
