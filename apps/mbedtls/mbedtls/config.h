73cd886bfd9deee07ab6923d84c796646b2d1b32
joriwind <j.winderickx@gmail.com>
2017-07-06 16:38:17 +0200

    fixes

--- apps/mbedtls/mbedtls/config.h
+++ apps/mbedtls/mbedtls/config.h
@@ -7,6 +7,15 @@
 /* Save RAM at the expense of ROM */
 #define MBEDTLS_AES_ROM_TABLES
 
+#define MBEDTLS_SHA256_C
+#define MBEDTLS_CCM_C
+#define MBEDTLS_MD_C
+#define MBEDTLS_CTR_DRBG_C
+#define MBEDTLS_AES_C
+#define MBEDTLS_CIPHER_C
+
+#define MBEDTLS_AES_ROM_TABLES
+
 
 #include "mbedtls/check_config.h"
 
