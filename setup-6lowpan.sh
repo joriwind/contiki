3ddee3534fd20b909476ef6e7c6ad39704936e3b
joriwind <j.winderickx@gmail.com>
2017-09-12 10:22:17 +0200

    setup without docker or login to docker

--- setup-6lowpan.sh
+++ setup-6lowpan.sh
@@ -1,16 +1,13 @@
 
 case $1 in
     border)
-        docker run -it --net=host --privileged -v $(pwd):/shared/ contikibuild-12 /bin/bash
-        wait 2
-        cd shared/examples/ipv6/rpl-border-router/
-        make connect-router
+        (cd examples/ipv6/rpl-border-router/ && sudo make connect-router)
         ;;
     node)
+        (cd examples/ipv6/rpl-border-router/ && sudo make login MOTES=/dev/ttyUSB1)
+        ;;
+    docker)
         docker run -it --net=host --privileged -v $(pwd):/shared/ contikibuild-12 /bin/bash
-        wait 2
-        cd shared/examples/ipv6/rpl-border-router/
-        make login MOTES=/dev/ttyUSB1
         ;;
     *)
         echo "No device given (border, node)"
